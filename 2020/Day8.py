ce9e159dbd64ead4fc27e27a371f2d3d54cd0480
Ryley <ryley.g@hotmail.com>
2022-04-22 13:46:16 -0600

    interim commit

--- 2020/Day8.py
+++ 2020/Day8.py
@@ -1,84 +1,68 @@
 #! python3
 
 with open('input-Day8.txt') as f:
-    inp = f.read().split('\n')
+	inp = f.read().strip().split('\n')
+
 
 def part_1(lines):
-    acc = 0
-    used_lines = []
-    line = 0
-    while line not in used_lines:
-        instruction = lines[line].split()
-
-        if instruction[0] == 'acc':
-            acc += int(instruction[1])
-            used_lines.append(line)
-            line += 1
-            continue
-        elif instruction[0] == 'nop':
-            used_lines.append(line)
-            line += 1
-            continue
-        elif instruction[0] == 'jmp':
-            used_lines.append(line)
-            line_jump = int(instruction[1])
-            line += line_jump
-            continue
-    return acc, used_lines[-20:]
-
-
-def check_line(check, used):
-    if check in used:
-        return True
-
-def part_2(lines):
-    acc = 0
-    used_lines = []
-    line = 0
-    change_line = None
-    instruction = True
-    while instruction:
-        instruction = lines[line].split()
-        command = instruction[0] if instruction else None
-        #print(line)
-        if command == 'acc':
-            acc += int(instruction[1])
-            used_lines.append(line)
-            line += 1
-            continue
-        elif command == 'nop':
-            if check_line(line + 1, used_lines) and change_line is None:
-                print(f'Changing line {line} from {command} to jmp')
-                command = 'jmp'
-                change_line = line
-                used_lines.append(line)
-                line += 1
-                continue
-            else:
-                used_lines.append(line)
-                line += 1
-                continue
-        elif command == 'jmp':
-            line_jump = int(instruction[1])
-            if check_line((line + line_jump), used_lines) and change_line is None:
-                print(f'Changing line {line} from {command} to nop')
-                command = 'nop'
-                change_line = line
-                used_lines.append(line)
-                line += 1
-                continue
-            else:
-                used_lines.append(line)
-                line += line_jump
-                continue
-        
-    return acc, change_line
+	acc = 0
+	used_lines = []
+	line = 0
+	while line not in used_lines:
+		try:
+			instruction, value = lines[line]
+		except KeyError:
+			break
+		if instruction == 'acc':
+			acc += value
+			used_lines.append(line)
+			line += 1
+			continue
+		elif instruction == 'nop':
+			used_lines.append(line)
+			line += 1
+			continue
+		elif instruction == 'jmp':
+			used_lines.append(line)
+			line_jump = value
+			line += line_jump
+			continue
+	return acc, used_lines
 
 
-#map = {}
-#for i, line in enumerate(lines):
- #   next_line = line.split()
-    #map[i] =
+def parse_lines(lines):
+	# parse the lines into a dict of line number and instructions, then we can use
+	# the dict in a loop to reverse every nop/jmp
+	line_instructions = {}
+	nop_jmp_lines = []
+	for index, line in enumerate(lines):
+		instruction, number = line.split()
+		line_instructions[index] = [instruction, int(number)]
+		if instruction in ('jmp', 'nop'):
+			nop_jmp_lines.append(index)
+	return nop_jmp_lines, line_instructions
 
-print(part_1(inp))
-print(part_2(inp))
+
+def part_2(swap_lines, instructions):
+	record = {}
+	zeroes = []
+	for swap in swap_lines:
+		swapped_instructions = instructions
+		if swapped_instructions[swap][0] == 'jmp':
+			swapped_instructions[swap][0] = 'nop'
+		else:
+			swapped_instructions[swap][0] = 'jmp'
+		value, used_lines = part_1(swapped_instructions)
+		record[swap] = value
+		if len(instructions.keys()) - 1 in used_lines:
+			zeroes.append(swap)
+	return value, swap
+
+
+swap_lines, instructions = parse_lines(inp)
+answerp1, used = part_1(instructions)
+print(answerp1)
+# try re-parsing the input into a dict with line numbers as keys
+
+# then brute force the solution by running accumulator for each changed instruction set
+print(part_2(swap_lines, instructions))
