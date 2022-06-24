48c39be06dc1dbc67fbda1780d5d7e9c58087e5f
Ryley Gray <ryley.g@hotmail.com>
2022-06-24 11:19:11 -0600

    day5 2016 complete

---
+++ 2016/day5part2.py
@@ -0,0 +1,37 @@
+import hashlib
+import time
+
+with open('./input/day5.txt') as f:
+    door_id = f.read()
+
+ZEROES = '00000'
+
+number = 0
+password_parts = [0, 0, 0, 0, 0, 0, 0, 0]
+start = time.time()
+counter = 0
+while 0 in password_parts:
+    next_idx = door_id + str(number)
+    idx_encoded = next_idx.encode()
+    md5_hash = hashlib.md5(idx_encoded)
+    hex = md5_hash.hexdigest()
+
+    if hex[0:5] == ZEROES:
+        next_char = str(hex[6])
+        char_idx = int(hex[5], 16)  # need to include base 16, not sure why
+        if char_idx < 8 and password_parts[char_idx] == 0:
+            password_parts[char_idx] = str(next_char)
+            print(password_parts)
+            counter += 1
+
+    current_time = time.time()
+    if current_time - start > 60:
+        print('Time exceeded')
+        break
+
+    number += 1
+
+end_time = time.time()
+password = ''.join(password_parts)
+print(password)
+print(end_time - start)
